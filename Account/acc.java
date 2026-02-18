3a08f72886415637ae83b3748095f0f204d82ce7
sushant-bit <sushantadhikari944@gmail.com>
2026-02-18 22:04:51 +0545

    Update acc.java

--- Account/acc.java
+++ Account/acc.java
@@ -3,5 +3,6 @@ package Account;
 public class acc {
     public static void main(String[] args) {
         System.out.println("This is Account Section");
+        System.out.println("Hello manoj");
     }
 }
