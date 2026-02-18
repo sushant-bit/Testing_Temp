721da31eb848e9ab322a98d11856574d969d0047
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-18 21:59:58 +0545

    Naya change from og branch

--- madan/hari.java
+++ madan/hari.java
@@ -4,6 +4,7 @@ public class hari {
     public static void main(String[] args) {
         System.out.println("Hello madan");
         System.out.println("Bhauju leh kaam pain");
+        System.out.println("Bhauju leh kaam pain aaba mithai khane");
         
     }
 }
