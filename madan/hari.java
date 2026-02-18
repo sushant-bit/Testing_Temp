c0fae57b4888054610b7b0a77b6f6e29f145cabc
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-18 21:56:19 +0545

    Changed something

--- madan/hari.java
+++ madan/hari.java
@@ -3,5 +3,7 @@ package madan;
 public class hari {
     public static void main(String[] args) {
         System.out.println("Hello madan");
+        System.out.println("Bhauju leh kaam pain");
+        
     }
 }
